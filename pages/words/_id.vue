2b3b91786f4be23a906fdbcd4d929ffd931118e9
shiki <shiki.tak@gmail.com>
2019-03-31 02:52:49 +0900

    try twitter card setting

--- pages/words/_id.vue
+++ pages/words/_id.vue
@@ -23,10 +23,10 @@ export default {
         return {
             meta: [
                 { hid: 'card', name: 'twitter:card', content: "summary_large_image" },
-                { hid: 'site', name: 'twitter:site', content: this.word.user_name },
+                { hid: 'site', name: 'twitter:site', content: "ジョージ・マロニー" },
                 { hid: 'title', name: 'twitter:title', content: "山の名言"},
                 { hid: 'description', name: 'twitter:description', content: "他の名言を見る" },
-                { hid: 'image', name: 'twitter:image', content: this.word.background_image_url }
+                { hid: 'image', name: 'twitter:image', content: "https://yamabluesky.files.wordpress.com/2019/01/slide01-2.jpg" }
             ]
         }
     }
