f1e3d2565fb939f6df12984af023b6ba6d304dcf
shiki <shiki.tak@gmail.com>
2019-03-31 00:56:10 +0900

    try twitter card setting

--- pages/words/_id.vue
+++ pages/words/_id.vue
@@ -25,13 +25,14 @@ export default {
 
     async head() {
         await this.getWord()
+        console.log(this.word.background_image_url)
         return {
             meta: [
                 { hid: 'card', name: 'twitter:card', content: "summary_large_image"},
                 { hid: 'site', name: 'twitter:site', content: "@shiki_developer"},
                 { hid: 'title', name: 'twitter:title', content: "山の名言"},
                 { hid: 'description', name: 'twitter:description', content: "他の名言を見る"},
-                { hid: 'image', name: 'twitter:image', content: this.word.background_image_url}
+                { hid: 'image', name: 'twitter:image', content: "https://yamabluesky.files.wordpress.com/2019/01/slide01-2.jpg"}
             ]
         }
     }
