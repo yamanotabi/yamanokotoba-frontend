d9a37bf10ad5471af1edfe2bf59f249acfdc0a07
shiki <shiki.tak@gmail.com>
2019-03-30 22:58:05 +0900

    try twitter card setting

--- pages/words/_id.vue
+++ pages/words/_id.vue
@@ -22,7 +22,7 @@ export default {
         return {
             meta: [
                 { hid: 'card', name: 'twitter:card', content: "summary_large_image"},
-                { hid: 'site', name: 'twitter:site', content: this.word.user_name},
+                { hid: 'site', name: 'twitter:site', content: "@shiki_developer"},
                 { hid: 'title', name: 'twitter:title', content: "山の名言"},
                 { hid: 'description', name: 'twitter:description', content: "他の名言を見る"},
                 { hid: 'image', name: 'twitter:image', content: this.word.background_image_url}
