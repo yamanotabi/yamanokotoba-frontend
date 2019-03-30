b32b9e7c1caa52f153a348aa7670a97cd6229173
shiki <shiki.tak@gmail.com>
2019-03-30 23:07:33 +0900

    try twitter card setting

--- pages/words/_id.vue
+++ pages/words/_id.vue
@@ -15,20 +15,24 @@ export default {
         }
     },
 
-    async head () {
-        const id = this.$route.params.id
-        const response = await axios.get("http://localhost:8080/api/v1/words/" + id)
-        this.word = response.data
+    head () {
         return {
             meta: [
                 { hid: 'card', name: 'twitter:card', content: "summary_large_image"},
                 { hid: 'site', name: 'twitter:site', content: "@shiki_developer"},
                 { hid: 'title', name: 'twitter:title', content: "山の名言"},
                 { hid: 'description', name: 'twitter:description', content: "他の名言を見る"},
-                { hid: 'image', name: 'twitter:image', content: "https://yamabluesky.files.wordpress.com/2019/01/slide01-2.jpg"}
+                { hid: 'image', name: 'twitter:image', content: "https://fathomless-oasis-51387.herokuapp.com/words/5c83d0b8ae4f418ddf6b004b"}
             ]
         }
-    },
+    }
+
+    // async mounted() {
+    //     const id = this.$route.params.id
+    //     const response = await axios.get("http://localhost:8080/api/v1/words/" + id)
+    //     this.word = response.data
+    //     console.log(this.word)
+    // }
 }
 </script>
 
