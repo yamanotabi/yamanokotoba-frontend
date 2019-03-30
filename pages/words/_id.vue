74387c9ec2180f8ecf52b08e666271d87793a11f
shiki <shiki.tak@gmail.com>
2019-03-30 23:42:21 +0900

    try twitter card setting

--- pages/words/_id.vue
+++ pages/words/_id.vue
@@ -15,24 +15,28 @@ export default {
         }
     },
 
-    head () {
+    // async mounted() {
+    //     const id = this.$route.params.id
+    //     const response = await axios.get("http://localhost:8080/api/v1/words/" + id)
+    //     this.word = response.data
+    //     // console.log(this.word)
+    // },
+
+    async head () {
+        const id = this.$route.params.id
+        const response = await axios.get("http://localhost:8080/api/v1/words/" + id)
+        this.word = response.data
+
         return {
             meta: [
                 { hid: 'card', name: 'twitter:card', content: "summary_large_image"},
                 { hid: 'site', name: 'twitter:site', content: "@shiki_developer"},
                 { hid: 'title', name: 'twitter:title', content: "山の名言"},
                 { hid: 'description', name: 'twitter:description', content: "他の名言を見る"},
-                { hid: 'image', name: 'twitter:image', content: "https://yamabluesky.files.wordpress.com/2019/01/slide01-2.jpg"}
+                { hid: 'image', name: 'twitter:image', content: this.word.background_image_url}
             ]
         }
     }
-
-    // async mounted() {
-    //     const id = this.$route.params.id
-    //     const response = await axios.get("http://localhost:8080/api/v1/words/" + id)
-    //     this.word = response.data
-    //     console.log(this.word)
-    // }
 }
 </script>
 
