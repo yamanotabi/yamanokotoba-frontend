05ed9bd8d0456811bbfd5a0e0672b8ad87135026
shiki <shiki.tak@gmail.com>
2019-03-31 02:47:08 +0900

    try twitter card setting

--- pages/words/_id.vue
+++ pages/words/_id.vue
@@ -15,43 +15,21 @@ export default {
         }
     },
 
-    methods: {
-        async getWord() {
-            const id = this.$route.params.id
-            const response = await axios.get("http://localhost:8080/api/v1/words/" + id)
-            this.word = response.data
-        }
-    },
+    async head () {
+        const id = this.$route.params.id
+        const response = await axios.get("http://localhost:8080/api/v1/words/" + id)
+        this.word = response.data
 
-    async head() {
-        await this.getWord()
-        console.log(this.word.background_image_url)
         return {
             meta: [
-                { hid: 'card', name: 'twitter:card', content: "summary_large_image"},
-                { hid: 'site', name: 'twitter:site', content: "@shiki_developer"},
+                { hid: 'card', name: 'twitter:card', content: "summary_large_image" },
+                { hid: 'site', name: 'twitter:site', content: this.word.user_name },
                 { hid: 'title', name: 'twitter:title', content: "山の名言"},
-                { hid: 'description', name: 'twitter:description', content: "他の名言を見る"},
-                { hid: 'image', name: 'twitter:image', content: "https://yamabluesky.files.wordpress.com/2019/01/slide01-2.jpg"}
+                { hid: 'description', name: 'twitter:description', content: "他の名言を見る" },
+                { hid: 'image', name: 'twitter:image', content: this.word.background_image_url }
             ]
         }
     }
-
-    // async head () {
-    //     const id = this.$route.params.id
-    //     const response = await axios.get("http://localhost:8080/api/v1/words/" + id)
-    //     this.word = response.data
-
-    //     return {
-    //         meta: [
-    //             { hid: 'card', name: 'twitter:card', content: "summary_large_image"},
-    //             { hid: 'site', name: 'twitter:site', content: "@shiki_developer"},
-    //             { hid: 'title', name: 'twitter:title', content: "山の名言"},
-    //             { hid: 'description', name: 'twitter:description', content: "他の名言を見る"},
-    //             { hid: 'image', name: 'twitter:image', content: this.word.background_image_url}
-    //         ]
-    //     }
-    // }
 }
 </script>
 
