32fb93c3a6d2c0b70233d973c37aeaad65c0fb0c
shiki <shiki.tak@gmail.com>
2019-04-04 23:13:47 +0900

    fix twitter card url

--- pages/words/_wordId.vue
+++ pages/words/_wordId.vue
@@ -25,7 +25,7 @@ export default {
                 { hid: 'card', name: 'twitter:card', content: "summary_large_image" },
                 { hid: 'site', name: 'twitter:site', content: `${this.$store.state.word.user_name}` },
                 { hid: 'title', name: 'twitter:title', content: "山の名言"},
-                { hid: 'url', name: 'twitter:url', content: process.env.BASE_URL },
+                { hid: 'url', name: 'twitter:url', content: 'https://yamagen.herokuapp.com' },
                 { hid: 'description', name: 'twitter:description', content: "他の名言を見る" },
                 { hid: 'image', name: 'twitter:image', content: `${this.$store.state.word.background_image_url}` }
             ]
