8df21fad79923d925948872eb4f4ff3dd952299d
shiki <shiki.tak@gmail.com>
2019-04-07 03:07:56 +0900

    fix url

--- pages/words/_wordId.vue
+++ pages/words/_wordId.vue
@@ -16,6 +16,7 @@ export default {
 
     async fetch ({ store, params }) {
         const response  = await this.$axios.get("/words/" + params.wordId)
+        console.log(response.data)
         store.commit('setWord', response.data)
     },
 
@@ -23,9 +24,7 @@ export default {
         return {
             meta: [
                 { hid: 'card', name: 'twitter:card', content: "summary_large_image" },
-                { hid: 'site', name: 'twitter:site', content: "@shiki_developer" },
-                { hid: 'url', name: 'twitter:url', content: 'https://yamagen.herokuapp.com' },
-                // { hid: 'site', name: 'twitter:site', content: `${this.$store.state.word.user_name}` },
+                { hid: 'site', name: 'twitter:site', content: `${this.$store.state.word.user_name}` },
                 { hid: 'title', name: 'twitter:title', content: "山の名言"},
                 { hid: 'description', name: 'twitter:description', content: "他の名言を見る" },
                 { hid: 'image', name: 'twitter:image', content: `${this.$store.state.word.background_image_url}` }
