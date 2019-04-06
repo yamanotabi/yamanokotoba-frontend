035ba2f407141ae02eb694231a9887d1c8568b5c
shiki <shiki.tak@gmail.com>
2019-04-07 02:50:02 +0900

    fix url

--- pages/words/_wordId.vue
+++ pages/words/_wordId.vue
@@ -23,7 +23,9 @@ export default {
         return {
             meta: [
                 { hid: 'card', name: 'twitter:card', content: "summary_large_image" },
-                { hid: 'site', name: 'twitter:site', content: `${this.$store.state.word.user_name}` },
+                { hid: 'site', name: 'twitter:site', content: "@shiki_developer" },
+                { hid: 'url', name: 'twitter:url', content: 'https://yamagen.herokuapp.com' },
+                // { hid: 'site', name: 'twitter:site', content: `${this.$store.state.word.user_name}` },
                 { hid: 'title', name: 'twitter:title', content: "山の名言"},
                 { hid: 'description', name: 'twitter:description', content: "他の名言を見る" },
                 { hid: 'image', name: 'twitter:image', content: `${this.$store.state.word.background_image_url}` }
