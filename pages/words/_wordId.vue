89b01ac20afa81ff1b02e367e5ee6645f4c53d4c
shiki <shiki.tak@gmail.com>
2019-04-07 12:13:35 +0900

    fix twitter card setting

--- pages/words/_wordId.vue
+++ pages/words/_wordId.vue
@@ -14,20 +14,20 @@ export default {
         }
     },
 
-    async fetch ({ store, params }) {
-        const response  = await this.$axios.get("/words/" + params.wordId)
-        store.commit('setWord', response.data)
-    },
+    // async fetch ({ store, params }) {
+    //     const response  = await this.$axios.get("/words/" + params.wordId)
+    //     store.commit('setWord', response.data)
+    // },
 
     head () {
         return {
-            title: this.$store.state.word.text,
+            // title: this.$store.state.word.text,
             meta: [
                 { hid: 'card', name: 'twitter:card', content: "summary_large_image" },
-                { hid: 'site', name: 'twitter:site', content: `${this.$store.state.word.user_name}` },
+                { hid: 'site', name: 'twitter:site', content: "@shiki_developer" },
                 { hid: 'title', name: 'twitter:title', content: "山の名言"},
                 { hid: 'description', name: 'twitter:description', content: "他の名言を見る" },
-                { hid: 'image', name: 'twitter:image', content: `${this.$store.state.word.background_image_url}` }
+                { hid: 'image', name: 'twitter:image', content: "https://s3-ap-northeast-1.amazonaws.com/yamatabi-production/images/test.jpg" }
             ]
         }
     }
